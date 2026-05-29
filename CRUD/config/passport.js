f3f67e098824a4f9f292d9a4b0c88e253818f09d
Santiago Benjamín Irigoyen <irigoyensantiago623@gmail.com>
2026-05-29 15:31:53 -0300

    Fixed Google OAuth callback URL

--- CRUD/config/passport.js
+++ CRUD/config/passport.js
@@ -14,7 +14,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      callbackURL:
+        process.env.NODE_ENV === 'production'
+          ? 'https://cse341-l7no.onrender.com/auth/google/callback'
+          : 'http://localhost:8080/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
