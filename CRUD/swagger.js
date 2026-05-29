0624a0195673725f27311707e1f44a101a3aadad
Santiago Benjamín Irigoyen <irigoyensantiago623@gmail.com>
2026-05-28 21:33:08 -0300

    28/5/2026

--- CRUD/swagger.js
+++ CRUD/swagger.js
@@ -1,16 +1,16 @@
-const swaggerAutogen = require("swagger-autogen")();
+const swaggerAutogen = require("swagger-autogen");
 
 const doc = {
   info: {
     title: "Games API",
     description: "API documentation for Games Project"
   },
-  host: "https://cse341-l7no.onrender.com/",
+  host: "cse341-l7no.onrender.com",
   basePath: "/games",
-  schemes: ["https","http"]
+  schemes: ["https"]
 };
 
 const outputFile = "./swagger.json";
 const routes = ["./server.js"];
 
-swaggerAutogen(outputFile, routes, doc);
+swaggerAutogen()(outputFile, routes, doc);
